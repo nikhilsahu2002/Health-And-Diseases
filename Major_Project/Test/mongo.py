751e833774e1523163b4e67fa062ca68cf7c548b
sagr-456 <sagarsgs456@gmail.com>
2023-05-01 16:59:57 +0530

    commited

--- Major_Project/Test/mongo.py
+++ Major_Project/Test/mongo.py
@@ -4,7 +4,6 @@ client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client['HealthAndDisease']
 collection = db['persion']
 
-
 def get_data():
     data = []
     for item in collection.find({"Symptom_1":" headache"}):
