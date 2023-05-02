e74f862afa0777b5b525c7411f18481de96cf693
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2023-05-02 23:48:50 +0530

    commit

--- Major_Project/Test/views.py
+++ Major_Project/Test/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import dataset
 # Create your views here.
 def home(request):
-    data = dataset.objects.name('Fungal infection')
+    data = dataset.objects.all().filter('Fungal infection')
     return render(request,'index.html',{'data':data})
